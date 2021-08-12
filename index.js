625e309d953b0d93f2a0bae28b909caaaa759b02
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-08-12 13:38:58 -0300

    chore: findAll findOne findByPk

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const db = require('./database/models');
 
-db.Aluno.findAll().then(rows => rows.map(row => row.dataValues)).then(console.log);
+const { log } = console;
+
+const run = async () => {
+  const print = input => {
+    const rows = [input].flat().map(row => row.dataValues);
+    log(rows);
+  }
+
+  log('Todos alunos');
+  log(''.padEnd(120, '-'));
+  const alunos = await db.Aluno.findAll();
+  print(alunos);
+
+  log('Rodrigo');
+  log(''.padEnd(120, '-'));
+  const rodrigo = await db.Aluno.findOne({ where: { nome: 'Rodrigo' }});
+  print(rodrigo);
+
+
+  log('Aluno.id = 1');
+  log(''.padEnd(120, '-'));
+  const aluno1 = await db.Aluno.findByPk(1);
+  print(aluno1);
+}
+
+run();
