0b226aad0a0900905328ddd46179fbe51e180194
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-08-12 13:47:31 -0300

    chore: like order limit offset

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const db = require('./database/models');
+const db = require("./database/models");
+const { Op } = require('sequelize');
 
 const { log } = console;
+const line = () => log("".padEnd(120, "-"));
 
 const run = async () => {
-  const print = input => {
-    const rows = [input].flat().map(row => row.dataValues);
+  const print = (input) => {
+    const rows = [input].flat().map((row) => row.dataValues);
     log(rows);
-  }
+  };
 
-  log('Todos alunos');
-  log(''.padEnd(120, '-'));
+  log("Todos alunos");
+  line();
   const alunos = await db.Aluno.findAll();
   print(alunos);
 
-  log('Rodrigo');
-  log(''.padEnd(120, '-'));
-  const rodrigo = await db.Aluno.findOne({ where: { nome: 'Rodrigo' }});
+  log("Rodrigo");
+  line();
+  const rodrigo = await db.Aluno.findOne({ where: { nome: "Rodrigo" } });
   print(rodrigo);
 
-
-  log('Aluno.id = 1');
-  log(''.padEnd(120, '-'));
+  log("Aluno.id = 1");
+  line();
   const aluno1 = await db.Aluno.findByPk(1);
   print(aluno1);
-}
+
+  log("Alunos que começam com a letra R");
+  line();
+  const alunosR = await db.Aluno.findAll({ where: { nome: {[Op.like]: 'R%' } }});
+  print(alunosR);
+
+  
+  log("Todos alunos desc");
+  line();
+  const alunosDesc = await db.Aluno.findAll({ order: [['nome', 'desc']], limit: 3, offset: 1 });
+  print(alunosDesc);
+};
 
 run();
